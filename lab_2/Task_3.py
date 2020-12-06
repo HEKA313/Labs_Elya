e1f9d026330119e12aac18eaf92df950594fc195
HEKA313 <52586811+HEKA313@users.noreply.github.com>
2020-12-06 17:22:55 +0300

    Create Отчет.docx

--- lab_2/Task_3.py
+++ lab_2/Task_3.py
@@ -1,15 +1,16 @@
 import math
 
-E = math.e
-A = 2.1
-B = 6.7
-x = float(input())
+for i in range(3):
+	E = math.e
+	A = 2.1
+	B = 6.7
+	x = float(input())
 
-if x < -2:
-	y = x * x * x + 2 * A
-elif -2 <= x <= 5:
-	y = math.exp(abs(math.cos(B * x)))
-else:
-	y = x * x * math.pow(E, x)
+	if x < -2:
+		y = x * x * x + 2 * A
+	elif -2 <= x <= 5:
+		y = math.exp(abs(math.cos(B * x)))
+	else:
+		y = x * x * math.pow(E, x)
 
-print(y)
+	print(y)
