8abd1e236eb3d6d4ba34dfcfdd5b91f3beba46e6
HEKA313 <52586811+HEKA313@users.noreply.github.com>
2020-12-06 15:54:10 +0300

    Update Task_3.py

--- lab_1/Task_3.py
+++ lab_1/Task_3.py
@@ -5,5 +5,7 @@ PI = math.pi
 a = float(input())
 
 z1 = math.pow(math.cos((3 / 8) * PI - a / 4), 2) - math.pow(math.cos(11 * PI / 8 + a / 4), 2)
+z2 = (math.sqrt(2) / 2 * math.sin(a / 2))
 
 print(z1)
+print(z2)
